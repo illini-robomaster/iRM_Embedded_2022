514f83d3f2613c1cda768dbb0fe3f9d75b69455f
youhy <haoyuan2019@outlook.com>
2021-12-26 14:15:24 -0800

    add rtos example

---
+++ src/example/rtos/typeA.cc
@@ -0,0 +1,77 @@
+/****************************************************************************
+ *                                                                          *
+ *  Copyright (C) 2020 RoboMaster.                                          *
+ *  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
+ *                                                                          *
+ *  This program is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  (at your option) any later version.                                     *
+ *                                                                          *
+ *  This program is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
+#include "bsp_gpio.h"
+#include "cmsis_os.h"
+#include "main.h"
+
+#define LED_RED_Pin GPIO_PIN_11
+#define LED_RED_GPIO_Port GPIOE
+
+#define LED_GREEN_Pin GPIO_PIN_14
+#define LED_GREEN_GPIO_Port GPIOF
+
+static bsp::GPIO *gpio_red, *gpio_green;
+
+/* init new task START */
+osThreadId_t LED_GREEN_TaskHandle;
+
+const osThreadAttr_t LED2Task_attributes = {
+  .name = "LEDGreenTask",
+  .attr_bits = osThreadDetached,
+  .cb_mem = NULL,
+  .cb_size = 0,
+  .stack_mem = NULL,
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+  .tz_module = 0,
+  .reserved = 0
+};
+
+void LED_GREEN_Task(void *argument) {
+  UNUSED(argument);
+  while (1) {
+    gpio_green->Toggle();
+    osDelay(200);
+  }
+}
+/* init new task END */
+
+
+void RM_RTOS_Threads_Init(void) {
+  LED_GREEN_TaskHandle = osThreadNew(LED_GREEN_Task, NULL, &LED2Task_attributes);
+}
+
+
+void RM_RTOS_Init(void) {
+  gpio_red = new bsp::GPIO(LED_RED_GPIO_Port, LED_RED_Pin);
+  gpio_green = new bsp::GPIO(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+  gpio_red->High();
+  gpio_green->Low();
+}
+
+void RM_RTOS_Default_Task(const void* args) {
+  UNUSED(args);
+  while (1) {
+    gpio_red->Toggle();
+    osDelay(500);
+  }
+}
+
