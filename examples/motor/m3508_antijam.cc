1a5f79d6716280db7119fce198f9705a95092f74
Eric Zhou <yiruiz2@illinois.edu>
2022-01-08 03:00:42 -0600

    add example && angle range modified

---
+++ examples/motor/m3508_antijam.cc
@@ -0,0 +1,90 @@
+/****************************************************************************
+ *                                                                          *
+ *  Copyright (C) 2022 RoboMaster.                                          *
+ *  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
+ *                                                                          *
+ *  This program is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  (at your option) any later version.                                     *
+ *                                                                          *
+ *  This program is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
+// #define WITH_CONTROLLER
+
+#include "bsp_gpio.h"
+#include "bsp_print.h"
+#include "cmsis_os.h"
+#include "controller.h"
+#include "main.h"
+#include "motor.h"
+#include "utils.h"
+
+
+#define KEY_GPIO_GROUP GPIOB
+#define KEY_GPIO_PIN GPIO_PIN_2
+
+#define NOTCH               (2 * PI / 4)
+#define SPEED               (2 * PI)
+
+bsp::CAN* can1 = nullptr;
+control::MotorCANBase* motor = nullptr;
+control::ServoMotor* servo = nullptr;
+BoolEdgeDetector key_detector(false);
+
+float target = NOTCH;
+
+void jam_callback(control::ServoMotor* servo) {
+  float prev = wrap<float>(servo->GetCurrentTarget() - NOTCH, 0, 2 * PI);
+  target = wrap<float>(target - NOTCH, 0, 2 * PI);
+  servo->SetTarget(prev, control::SERVO_CLOCKWISE, true);
+  print("ACTIVATE\r\n");
+  print("ATAR: %10.4f\r\n", target);
+}
+
+void RM_RTOS_Init() {
+  print_use_uart(&huart8);
+  can1 = new bsp::CAN(&hcan1, 0x201);
+  motor = new control::Motor3508(can1, 0x201);
+
+  control::servo_t servo_data;
+  servo_data.motor = motor;
+  servo_data.mode = control::SERVO_ANTICLOCKWISE;
+  servo_data.speed = SPEED;
+  servo_data.transmission_ratio = M3508P19_RATIO;
+  servo_data.move_Kp = 30;
+  servo_data.move_Ki = 10;
+  servo_data.move_Kd = 40;
+  servo_data.hold_Kp = 1500;
+  servo_data.hold_Ki = 15;
+  servo_data.hold_Kd = 100;
+  servo = new control::ServoMotor(servo_data);
+
+  servo->RegisterJamCallback(jam_callback, 50, 0.6);
+}
+
+void RM_RTOS_Default_Task(const void* args) {
+  UNUSED(args);
+
+  control::MotorCANBase* motors[] = {motor};
+  bsp::GPIO key(KEY_GPIO_GROUP, GPIO_PIN_2);
+
+  while (true) {
+    key_detector.input(key.Read());
+    if (key_detector.posEdge() && servo->SetTarget(target) != 0) {
+      print("TAR: %10.4f\r\n", target);
+      target = wrap<float>(target + NOTCH, 0, 2 * PI);
+    }
+    servo->CalcOutput();
+    control::MotorCANBase::TransmitOutput(motors, 1);
+    osDelay(10);
+  }
+}
