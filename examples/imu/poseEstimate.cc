501dbf91d7ffdad1c5352a6b2d85c270515d6802
AzulRadio <50132891+AzulRadio@users.noreply.github.com>
2022-01-12 18:50:17 -0600

    Add Pose Estimator Class and Example (#13)
    * pose estimation skeleton
    
    * add estimate example
    
    * temp update pose
    
    * stack size change && ioc update
    
    * add pose estimator example
    
    * update comments and fix according to change requested
    
    * revert usbd_cdc.c
    
    * Update shared/libraries/pose.cc
    
    * add naive yaw estimation
    
    Co-authored-by: Eric Zhou <52446536+0-EricZhou-0@users.noreply.github.com>
    
    Co-authored-by: Eric Zhou <yiruiz2@illinois.edu>
    Co-authored-by: Eric Zhou <52446536+0-EricZhou-0@users.noreply.github.com>

---
+++ examples/imu/poseEstimate.cc
@@ -0,0 +1,103 @@
+/****************************************************************************
+ *                                                                          *
+ *  Copyright (C) 2022 RoboMaster.                                          *
+ *  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
+ *                                                                          *
+ *  This program is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  (at your option) any later version.                                     *
+ *                                                                          *
+ *  This program is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
+#include <cmath>
+
+#include "main.h"
+
+#include "bsp_gpio.h"
+#include "bsp_imu.h"
+#include "bsp_os.h"
+#include "cmsis_os.h"
+#include "bsp_print.h"
+
+#include "pose.h"
+
+#define ONBOARD_IMU_SPI hspi5
+#define ONBOARD_IMU_CS_GROUP GPIOF
+#define ONBOARD_IMU_CS_PIN GPIO_PIN_6
+#define PRING_UART huart8
+
+static bsp::MPU6500* imu;
+
+void RM_RTOS_Init(void) {
+	bsp::SetHighresClockTimer(&htim2);
+	print_use_uart(&PRING_UART);
+}
+
+void RM_RTOS_Default_Task(const void* arguments) {
+  UNUSED(arguments);
+  
+  // init IMU instance
+  bsp::GPIO chip_select(ONBOARD_IMU_CS_GROUP, ONBOARD_IMU_CS_PIN);
+  imu = new bsp::MPU6500(&ONBOARD_IMU_SPI, chip_select, MPU6500_IT_Pin);
+  osDelay(3000);
+  print("IMU Initialized!\r\n");
+    
+  // init pose estimator instance
+  control::Pose poseEstimator(imu);
+  
+  // Set alpha for the complementary filter in the pose estimator
+  poseEstimator.SetAlpha(0.95);
+  
+  // init params
+  float roll = 0;
+  float pitch = 0;
+  float yaw = 0;
+  uint32_t i = 0;
+  float degRoll = 0;
+  float degPitch = 0;
+  float degYaw = 0;
+  // calibrate the Offset for IMU acce meter and gyro
+  poseEstimator.Calibrate();
+  
+  // reset timer and pose for IMU
+  poseEstimator.PoseInit();
+  
+  /* NOTE: IMU SHOULD BE PLACED ON A FLAT PLANE WHILE RUNNING THE FUNCTIONS ABOVE */
+  
+  while (true) {
+    // update estimated pose with complementary filter
+    poseEstimator.ComplementaryFilterUpdate();
+    osDelay(10);
+    
+    // print pose every 200ms
+    i += 1;
+    if (i >= 20) {
+      set_cursor(0, 0);
+      clear_screen();
+      // get roll and pitch, in RAD
+      roll = poseEstimator.GetRoll();
+      pitch = poseEstimator.GetPitch();
+      yaw = poseEstimator.GetYaw();
+      print("PITCH: %6.4f ROLL: %6.4f \r\n", pitch, roll);
+      print("YAW: %6.4f \r\n", yaw);
+      // convert to DEG
+      degRoll = roll * 180 / 3.14;
+      degPitch = pitch * 180 / 3.14;
+      degYaw = yaw * 180 / 3.14;
+      print("P_DEG: %6.4f R_DEG: %6.4f \r\n", degPitch, degRoll);
+      print("Y_DEG: %6.4f \r\n", degYaw);
+      
+      i = 0;
+    }
+    
+  }
+}
---
+++ shared/libraries/pose.h
@@ -0,0 +1,122 @@
+/****************************************************************************
+ *                                                                          *
+ *  Copyright (C) 2022 RoboMaster.                                          *
+ *  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
+ *                                                                          *
+ *  This program is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  (at your option) any later version.                                     *
+ *                                                                          *
+ *  This program is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+ 
+#pragma once
+
+#include "bsp_imu.h"
+
+namespace control {
+
+class Pose {
+public:
+  /** @description: constructor for Pose
+   *  @param: _imu: imu pointer
+  **/
+  Pose(bsp::MPU6500* _imu);
+
+  /** @description: reset all estimated pose to 0 and refresh timestamp
+   *  
+  **/
+  void PoseInit(void);
+
+  /** @description: naive calibration for all offsets
+   *                read from IMU and compute the average for 100 times
+   *  @param: num, IMU reads to compute the average offset
+   *  @note : by default takes 1s to calibrate
+  **/
+  void Calibrate(void);
+
+  /** @description: naive calibration for all offsets
+   *                read from IMU and compute the average for given times
+   *  @param: num, IMU reads to compute the average offset
+   *  @note : it takes _num * 10ms to calibrate
+  **/
+  void Calibrate(int16_t _num);
+
+  /** @description: naive estimation for gravity
+   *  @note : average 100 measure of z acceleration. Takes 1s.
+  **/
+  float GetGravity(void);
+
+  /** @description: set offset (bias correction) for the 6 inputs
+   *  @param: Offset (Bias) for the 3 axis of acce and gyro
+   *  @note: The mean of all 6 axis except acc_z_off should be 0 after setting this.
+   *         NOTE: acc_z_off is suggested to be set to 0, 
+   *               see pose.cc Calibrate() for detail.
+  **/
+  void SetOffset(float acc_x_off, float acc_y_off, float acc_z_off, 
+                 float gyro_x_off, float gyro_y_off, float gyro_z_off);
+
+  /** @description: read estimated pitch in RAD
+   *  @note: this error gets larger as ROLL gets larger. When ROLL get close to +/-90 deg, this value is not usable.
+  **/
+  float GetPitch(void);
+
+  /** @description: read estimated roll in RAD
+   *  @note: this error gets larger as PITCH gets larger. When PITCH get close to +/-90 deg, this value is not usable.
+  **/
+  float GetRoll(void);
+  
+  /** @description: read estimated yaw in RAD
+   *  @note: this function is only based on gyro and it will drift.
+  **/
+  float GetYaw(void);
+  
+  /** @description: set complementary filter weight
+   *  @note: should be close to 1.0
+  **/
+  void SetAlpha(float _alpha);
+  
+  /** @description: update pose with complementary filter from IMU's gyro and acce
+   *  @note: delay between 2 ComplementaryFilterUpdate() calls should be as small as possible
+   *         ideally ~10ms
+   *  @reference: https://www.pieter-jan.com/node/11
+  **/
+  void ComplementaryFilterUpdate(void);
+
+private:
+  bsp::MPU6500* imu;
+  // last pose
+  float pitch;
+  float roll;
+  float yaw;
+  
+  // noise bias correction offset for 3 axis of acce and gyro
+  float acc_x_off;
+  float acc_y_off;
+  float acc_z_off;
+  float gyro_x_off;
+  float gyro_y_off;
+  float gyro_z_off;
+  
+  // last timestamp
+  uint32_t timestamp;
+  
+  // alpha used in complementary filter
+  float alpha;
+  
+  // pose estimated by acce alone
+  float pitchAcc;
+  float rollAcc;
+   
+}; // class Pose end
+
+
+} /* namespace control */
