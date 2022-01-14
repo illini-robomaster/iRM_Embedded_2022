3557bd6d2027ba8d4b23f2b6b44849595981861c
Alchemic Ronin 炼金浪人 <1216532005@qq.com>
2022-01-13 18:32:32 -0600

    add bsp_relay with example

---
+++ examples/relay/main.cc
@@ -0,0 +1,48 @@
+/****************************************************************************
+ *                                                                          *
+ *  Copyright (C) 2022 RoboMaster.                                          *
+ *  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
+ *                                                                          *
+ *  This program is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  (at your option) any later version.                                     *
+ *                                                                          *
+ *  This program is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
+#include "bsp_relay.h"
+#include "bsp_print.h"
+#include "cmsis_os.h"
+#include "main.h"
+
+static bsp::Relay* relay;
+
+void RM_RTOS_Init(void) {
+	print_use_uart(&huart8);
+	relay = new bsp::Relay(L2_OUTPUT_GPIO_Port, L2_OUTPUT_Pin);
+}
+
+void RM_RTOS_Default_Task(const void* arguments) {
+	UNUSED(arguments);
+
+	while (true) {
+		set_cursor(0, 0);
+		clear_screen();
+		relay->On();
+		print("relay on\r\n");
+		osDelay(1000);
+		set_cursor(0, 0);
+		clear_screen();
+		relay->Off();
+		print("relay off\r\n");
+		osDelay(1000);
+	}
+}
---
+++ shared/bsp/bsp_relay.h
@@ -0,0 +1,52 @@
+/****************************************************************************
+ *                                                                          *
+ *  Copyright (C) 2022 RoboMaster.                                          *
+ *  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
+ *                                                                          *
+ *  This program is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  (at your option) any later version.                                     *
+ *                                                                          *
+ *  This program is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
+#pragma once
+
+#include "bsp_error_handler.h"
+#include "bsp_gpio.h"
+#include "main.h"
+
+namespace bsp {
+
+class Relay{
+ public:
+  /**
+   * @brief Constructor for relay
+   *
+   * @param relay_group relay GPIO group
+   * @param relay_pin   relay GPIO pin number
+   */
+  Relay(GPIO_TypeDef* relay_group, uint16_t relay_pin);
+
+  /**
+   * @brief Turn on relay
+   */
+  void On();
+
+  /**
+   * @brief Turn off relay
+   */
+  void Off();
+ private:
+  GPIO relay_;
+};
+
+} /* namespace bsp */
