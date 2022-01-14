265cf9eac20f4e53e4da6baa183494cdb6c64b91
Eric Zhou <yiruiz2@illinois.edu>
2022-01-14 03:38:13 -0600

    change more and more tab to space

--- examples/chassis/main.cc
+++ examples/chassis/main.cc
@@ -34,44 +34,44 @@ control::Chassis* chassis = nullptr;
 remote::DBUS* dbus = nullptr;
 
 void RM_RTOS_Init() {
-	print_use_uart(&huart8);
-	can = new bsp::CAN(&hcan1, 0x200);
-	fl_motor = new control::Motor3508(can, 0x202);
-	fr_motor = new control::Motor3508(can, 0x201);
-	bl_motor = new control::Motor3508(can, 0x203);
-	br_motor = new control::Motor3508(can, 0x204);
+  print_use_uart(&huart8);
+  can = new bsp::CAN(&hcan1, 0x200);
+  fl_motor = new control::Motor3508(can, 0x202);
+  fr_motor = new control::Motor3508(can, 0x201);
+  bl_motor = new control::Motor3508(can, 0x203);
+  br_motor = new control::Motor3508(can, 0x204);
 
   control::MotorCANBase* motors[control::FourWheel::motor_num];
-	motors[control::FourWheel::front_left] = fl_motor;
-	motors[control::FourWheel::front_right] = fr_motor;
-	motors[control::FourWheel::back_left] = bl_motor;
-	motors[control::FourWheel::back_right] = br_motor;
+  motors[control::FourWheel::front_left] = fl_motor;
+  motors[control::FourWheel::front_right] = fr_motor;
+  motors[control::FourWheel::back_left] = bl_motor;
+  motors[control::FourWheel::back_right] = br_motor;
 
-	control::chassis_t chassis_data;
-	chassis_data.motors = motors;
-	chassis_data.model = control::CHASSIS_STANDARD_ZERO;
-	chassis = new control::Chassis(chassis_data);
+  control::chassis_t chassis_data;
+  chassis_data.motors = motors;
+  chassis_data.model = control::CHASSIS_STANDARD_ZERO;
+  chassis = new control::Chassis(chassis_data);
 
-	dbus = new remote::DBUS(&huart1);
+  dbus = new remote::DBUS(&huart1);
 }
 
 void RM_RTOS_Default_Task(const void* args) {
-	UNUSED(args);
+  UNUSED(args);
 
-	osDelay(500); // DBUS initialization needs time
+  osDelay(500); // DBUS initialization needs time
 
   control::MotorCANBase* motors[] = {fl_motor, fr_motor, bl_motor, br_motor};
 
-	while (true) {
-		chassis->SetSpeed(dbus->ch0, dbus->ch1, dbus->ch2);
+  while (true) {
+    chassis->SetSpeed(dbus->ch0, dbus->ch1, dbus->ch2);
 
     // Kill switch
     if (dbus->swl == remote::UP || dbus->swl == remote::DOWN) {
       RM_ASSERT_TRUE(false, "Operation killed");
     }
 
-		chassis->Update();
+    chassis->Update();
     control::MotorCANBase::TransmitOutput(motors, 4);
-		osDelay(10);
-	}
+    osDelay(10);
+  }
 }
