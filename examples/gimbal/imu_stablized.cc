36683f239da039a0e171c9568b26539790e3fe99
youhy <haoyuan2019@outlook.com>
2022-01-13 17:12:50 -0800

    imu based gimbal stablize

--- examples/gimbal/imu_stablized.cc
+++ examples/gimbal/imu_stablized.cc
@@ -25,27 +25,95 @@
 #include "gimbal.h"
 #include "dbus.h"
 
+#include "pose.h"
 
+// init gimbal
 #define KEY_GPIO_GROUP GPIOB
 #define KEY_GPIO_PIN GPIO_PIN_2
-
 #define NOTCH         (2 * PI / 8)
 #define SERVO_SPEED   (PI)
 
 bsp::CAN* can = nullptr;
 control::MotorCANBase* pitch_motor = nullptr;
 control::MotorCANBase* yaw_motor = nullptr;
-
 control::Gimbal* gimbal = nullptr;
 remote::DBUS* dbus = nullptr;
 bool status = false;
 
+
+// init imu
+#define ONBOARD_IMU_SPI hspi5
+#define ONBOARD_IMU_CS_GROUP GPIOF
+#define ONBOARD_IMU_CS_PIN GPIO_PIN_6
+#define PRING_UART huart8
+
+static bsp::MPU6500* imu;
+
+static float roll = 0;
+static float pitch = 0;
+static float yaw = 0;
+
+static control::Pose *poseEstimator;
+
+/* init IMU task START */
+
+/*
+static osMutexId_t rpyLockHandle;
+
+const osMutexAttr_t rpyLock = {
+  .name = "rpyLock",
+  .attr_bits = osMutexRecursive,
+  .cb_mem = nullptr,
+  .cb_size = 0
+};
+*/
+
+static osThreadId_t IMUTaskHandle;
+
+const osThreadAttr_t IMUTaskAttributes = {
+  .name = "AddTask",
+  .attr_bits = osThreadDetached,
+  .cb_mem = nullptr,
+  .cb_size = 0,
+  .stack_mem = nullptr,
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+  .tz_module = 0,
+  .reserved = 0
+};
+
+// pose estimation task
+void IMU_Task(void *argument) {
+  UNUSED(argument);
+
+
+  while (true) {
+    // update estimated pose with complementary filter
+    poseEstimator->ComplementaryFilterUpdate();
+    osDelay(10);
+  }
+} /* IMU task ends */
+
+
+/* init IMU task END */
+
+
+/*
+void RM_RTOS_Mutexes_Init(void) {
+  rpyLockHandle = osMutexNew (&rpyLock);
+}
+*/
+void RM_RTOS_Threads_Init(void) {
+  IMUTaskHandle = osThreadNew(IMU_Task, nullptr, &IMUTaskAttributes);
+}
+
 void RM_RTOS_Init() {
   print_use_uart(&huart8);
+  
+  // init gimbal
   can = new bsp::CAN(&hcan1, 0x205);
   pitch_motor = new control::Motor6020(can, 0x205);
   yaw_motor = new control::Motor6020(can, 0x206);
-
   control::gimbal_t gimbal_data;
   gimbal_data.pitch_motor = pitch_motor;
   gimbal_data.yaw_motor = yaw_motor;
@@ -70,29 +138,50 @@ void RM_RTOS_Init() {
   gimbal = new control::Gimbal(gimbal_data);
 
   dbus = new remote::DBUS(&huart1);
+  
+  // IMU init
+  bsp::GPIO chip_select(ONBOARD_IMU_CS_GROUP, ONBOARD_IMU_CS_PIN);
+  imu = new bsp::MPU6500(&ONBOARD_IMU_SPI, chip_select, MPU6500_IT_Pin);
+  osDelay(3000);
+  print("IMU Initialized!\r\n");
+  
+  // init pose estimator instance
+  poseEstimator = new control::Pose(imu);
+  
+  // Set alpha for the complementary filter in the pose estimator
+  poseEstimator->SetAlpha(0.95);
+  
+  roll = 0;
+  pitch = 0;
+  yaw = 0;
+  
+  // calibrate the Offset for IMU acce meter and gyro
+  // Need the gimbal to be stable for 1sec
+  poseEstimator->Calibrate();
+  
+  // reset timer and pose for IMU
+  poseEstimator->PoseInit();
+
+  
 }
 
+
+// Gimbal task
 void RM_RTOS_Default_Task(const void* args) {
   UNUSED(args);
-
+	
 	osDelay(500); // DBUS initialization needs time
   
   control::MotorCANBase* motors[] = {pitch_motor, yaw_motor};
 
   while (true) {
-    float pitch_ratio = -dbus->ch3 / 600.0;
-    float yaw_ratio = -dbus->ch2 / 600.0;
-    if (dbus->swr == remote::UP) {
-      gimbal->TargetAbs(pitch_ratio * LEGACY_GIMBAL_PMAX, yaw_ratio * LEGACY_GIMBAL_YMAX);
-    } else if (dbus->swr == remote::MID) {
-      gimbal->TargetRel(pitch_ratio / 50, yaw_ratio / 50);
-    } 
+    gimbal->TargetRel(-pitch / 50, -yaw / 50);
     
-    // Kill switch
-    if (dbus->swl == remote::UP || dbus->swl == remote::DOWN) {
-      RM_ASSERT_TRUE(false, "Operation killed");
-    }
-
+    // update rpy
+    yaw = poseEstimator->GetYaw();
+    pitch = poseEstimator->GetPitch();
+    roll = poseEstimator->GetRoll();
+    
     gimbal->CalcOutput();
     control::MotorCANBase::TransmitOutput(motors, 2);
     osDelay(10);
