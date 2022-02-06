d9981fea25aa3ba46acd332fc0d3d77879aeb678
Alchemic Ronin 炼金浪人 <1216532005@qq.com>
2022-02-05 22:24:35 -0600

    semiwork

--- shared/libraries/robot_arm.h
+++ shared/libraries/robot_arm.h
@@ -19,50 +19,27 @@
  *                                                                          *
  ****************************************************************************/
 #pragma once
-#define WITH_CONTROLLER
-#include "bsp_gpio.h"
-#include "bsp_print.h"
-#include "cmsis_os.h"
+
 #include "controller.h"
-#include "main.h"
 #include "motor.h"
-#ifdef WITH_CONTROLLER
-#include "dbus.h"
-#endif
-#define KEY_GPIO_GROUP GPIOB
-#define KEY_GPIO_PIN GPIO_PIN_2
-#define SPEED (4 * PI)
-#define ACCELERATION (2 * PI)
-#define MAX_MOTOR_NUM 3
-
-#include "bsp_laser.h"
-#include "bsp_print.h"
-#include "cmsis_os.h"
-
-#ifndef IRM_EMBEDDED_2022_ROBOT_ARM_H
-#define IRM_EMBEDDED_2022_ROBOT_ARM_H
 
 namespace control {
-  typedef enum { ROBOT_ARM_STANDARD_ZERO } robot_arm_model_t;
-
   typedef struct {
     MotorCANBase** motors; /* motor instances of all robot_arm motors */
-    robot_arm_model_t model;
-
   } robot_arm_t;
 
   struct Motors {
     enum {left, right, gripper};
   };
 
-  struct Servos {
-    enum {left, right, gripper};
-  };
+//  struct Servos {
+//    enum {left, right, gripper};
+//  };
 
-class Robot_arm {
+class RobotArm {
   public:
-    Robot_arm(const robot_arm_t robot_arm);
-    ~Robot_arm();
+    RobotArm(const robot_arm_t robot_arm);
+    ~RobotArm();
 
  private:
     MotorCANBase** motors_;
@@ -71,6 +48,3 @@ class Robot_arm {
     float* speeds_;
   };
 }
-
-
-#endif  // IRM_EMBEDDED_2022_ROBOT_ARM_H
