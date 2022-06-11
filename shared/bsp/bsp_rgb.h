c11bcf28e041e16746716ddf86e920655730f922
AlchemicRonin <1216532005@qq.com>
2022-06-11 16:48:41 -0500

    add RGB lighting effect for typeC

---
+++ shared/bsp/bsp_rgb.h
@@ -0,0 +1,36 @@
+/****************************************************************************
+*                                                                          *
+*  Copyright (C) 2022 RoboMaster.                                          *
+*  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
+*                                                                          *
+*  This program is free software: you can redistribute it and/or modify    *
+*  it under the terms of the GNU General Public License as published by    *
+*  the Free Software Foundation, either version 3 of the License, or       *
+*  (at your option) any later version.                                     *
+*                                                                          *
+*  This program is distributed in the hope that it will be useful,         *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+*  GNU General Public License for more details.                            *
+*                                                                          *
+*  You should have received a copy of the GNU General Public License       *
+*  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+*                                                                          *
+****************************************************************************/
+
+#pragma once
+
+#include "bsp_pwm.h"
+
+namespace bsp {
+
+class RGB {
+ public:
+  RGB(TIM_HandleTypeDef* htim, uint8_t channelR, uint8_t channelG, uint8_t channelB, uint32_t clock_freq);
+  void Show(uint32_t aRGB);
+  void Stop();
+ private:
+  PWM R_, G_, B_;
+};
+
+}
