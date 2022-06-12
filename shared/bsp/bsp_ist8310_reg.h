5c85db8848ac95d2850b65384390bb06cb0884da
Alvin Sun <alvinsunyixiao@gmail.com>
2022-06-11 22:33:20 -0700

    forgot a few files

---
+++ shared/bsp/bsp_ist8310_reg.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define IST8310_WHO_AM_I 0x00       //ist8310 "who am I "
+#define IST8310_WHO_AM_I_VALUE 0x10 //device ID
+
+// TODO(alvin): add more register stuff here
+#define IST8310_DATAXL  0x03
