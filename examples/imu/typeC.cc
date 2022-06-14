3755078cf8972d562b9235c9ee4dace39229fb40
Alvin Sun <alvinsunyixiao@gmail.com>
2022-06-14 10:31:57 -0700

    update example

--- examples/imu/typeC.cc
+++ examples/imu/typeC.cc
@@ -44,17 +44,11 @@ void RM_RTOS_Default_Task(const void* arguments) {
   while (true) {
     set_cursor(0, 0);
     clear_screen();
-    print("Mag:: %.1f, %.1f, %.1f\r\n", IST8310->mag[0], IST8310->mag[1], IST8310->mag[2]);
 
-    //float accel[3], gyro[3], temp;
-    //BMI088->Read(gyro, accel, &temp);
-    //print("IMU::\r\ngyro %.1f %.1f %.1f %lu\r\naccel %.1f %.1f %.1f %lu\r\ntemp: %.1f",
-    //      gyro[0], gyro[1], gyro[2], BMI088->gyro_timestamp,
-    //      accel[0], accel[1], accel[2], BMI088->accel_timestamp,
-    //      temp);
     print("IMU::\r\ngyro %.1f %.1f %.1f %lu\r\naccel %.1f %.1f %.1f %lu\r\n",
           BMI088->gyro.x(), BMI088->gyro.y(), BMI088->gyro.z(), BMI088->gyro_timestamp,
           BMI088->accel.x(), BMI088->accel.y(), BMI088->accel.z(), BMI088->accel_timestamp);
+    print("Mag:: %.1f, %.1f, %.1f\r\n", IST8310->mag[0], IST8310->mag[1], IST8310->mag[2]);
     osDelay(100);
   }
 }
