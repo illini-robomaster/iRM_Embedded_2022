4ae710dc2ccfe661a3d4aea988931cb0eebdced3
Alvin Sun <alvinsunyixiao@gmail.com>
2022-06-13 18:53:26 -0700

    change i2c priority

--- boards/DJI_Board_TypeC/Core/Src/i2c.c
+++ boards/DJI_Board_TypeC/Core/Src/i2c.c
@@ -154,7 +154,7 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
     hdma_i2c3_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
     hdma_i2c3_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
     hdma_i2c3_rx.Init.Mode = DMA_NORMAL;
-    hdma_i2c3_rx.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_i2c3_rx.Init.Priority = DMA_PRIORITY_HIGH;
     hdma_i2c3_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
     if (HAL_DMA_Init(&hdma_i2c3_rx) != HAL_OK)
     {
