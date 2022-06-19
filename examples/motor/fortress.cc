3cd208a9f7392e268aa3f597bf71673d171884e6
davidxiong <daweix3@illinois.edu>
2022-06-19 00:33:53 -0500

    add fortress.cc

---
+++ examples/motor/fortress.cc
@@ -0,0 +1,97 @@
+/****************************************************************************
+*                                                                          *
+*  Copyright (C) 2022 RoboMaster.                                          *
+*  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
+*                                                                          *
+*  This program is free software: you can redistribute it and/or modify    *
+*  it under the terms of the GNU General Public License as published by    *
+*  the Free Software Foundation, either version 3 of the License, or       *
+*  (at your option) any later version.                                     *
+*                                                                          *
+*  This program is distributed in the hope that it will be useful,         *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+*  GNU General Public License for more details.                            *
+*                                                                          *
+*  You should have received a copy of the GNU General Public License       *
+*  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+*                                                                          *
+****************************************************************************/
+
+// #define WITH_CONTROLLER
+
+#include "bsp_gpio.h"
+#include "bsp_print.h"
+#include "bsp_os.h"
+#include "cmsis_os.h"
+#include "controller.h"
+#include "main.h"
+#include "motor.h"
+#include "utils.h"
+
+#define KEY_GPIO_GROUP GPIOB
+#define KEY_GPIO_PIN GPIO_PIN_2
+
+#define NOTCH (2 * PI / 4)
+#define SPEED (2 * PI)
+#define ACCELERATION (20 * PI)
+
+bsp::CAN* can1 = nullptr;
+//bsp::CAN* can2 = nullptr;
+control::MotorCANBase* motor = nullptr;
+control::ServoMotor* servo = nullptr;
+BoolEdgeDetector key_detector(false);
+
+void jam_callback(control::ServoMotor* servo, const control::servo_jam_t data) {
+ UNUSED(data);
+ float servo_target = servo->GetTarget();
+ if (servo_target < servo->GetTheta()) {
+   print("Antijam in operation\r\n");
+ } else {
+   float prev_target = servo->GetTarget() - NOTCH;
+   servo->SetTarget(prev_target, true);
+   print("Antijam engage\r\n");
+ }
+}
+
+void RM_RTOS_Init() {
+ print_use_uart(&huart8);
+ bsp::SetHighresClockTimer(&htim2);
+
+ can1 = new bsp::CAN(&hcan2, 0x201, false);
+ motor = new control::Motor3508(can1, 0x202);
+
+ control::servo_t servo_data;
+ servo_data.motor = motor;
+ servo_data.max_speed = SPEED;
+ servo_data.max_acceleration = ACCELERATION;
+ servo_data.transmission_ratio = M3508P19_RATIO;
+ servo_data.omega_pid_param = new float[3]{60, 0.5, 100};
+ servo = new control::ServoMotor(servo_data);
+
+ servo->RegisterJamCallback(jam_callback, 0.6);
+}
+
+void RM_RTOS_Default_Task(const void* args) {
+ UNUSED(args);
+
+ control::MotorCANBase* motors[] = {motor};
+ bsp::GPIO key(KEY_GPIO_GROUP, GPIO_PIN_2);
+ int mul = 1;
+
+ while (true) {
+   print("Servomotor target is: %8.4f\r\n", mul);
+
+   if (key.Read()) {
+     servo->SetTarget(2*PI);
+     //print("Servomotor step forward, target: %8.4f\r\n", servo->GetTarget());
+     //mul++;
+   } else if (key.Read() == 0){
+     servo->SetTarget(0);
+     servo->GetTarget();
+   }
+   servo->CalcOutput();
+   control::MotorCANBase::TransmitOutput(motors, 1);
+   osDelay(2);
+ }
+}
